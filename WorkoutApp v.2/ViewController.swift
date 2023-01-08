dfadfdd1a87770bd6260884c94ba9f854b0afb52
CostOfSociety <48133616+CostOfSociety@users.noreply.github.com>
2023-01-08 14:33:25 +0300

    Initial Commit

---
+++ WorkoutApp v.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutApp v.2
+//
+//  Created by Святослав on 08.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
